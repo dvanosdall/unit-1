ac065502ef8bff59c9daf4c23f63facb5cb2f0a9
dvanosdall <dvanosdall15@gmail.com>
2025-02-04 17:46:06 -0600

    updated backColor to color

--- Chapter02/js/main_with_debug.js
+++ Chapter02/js/main_with_debug.js
@@ -92,7 +92,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
 
             // Set the table's background color to the random RGB value
-            document.querySelector('table').style.backgroundColor = color;
+            document.querySelector('table').style.color = color;
         });
 
         // Show a pop-up message when the table is clicked
